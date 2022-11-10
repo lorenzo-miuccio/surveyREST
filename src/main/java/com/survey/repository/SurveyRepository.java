df0c2f161629f384954153642bde9ad9fc00b060
lorenzo-miuccio <lorenzo.miuccio@gmail.com>
2022-11-10 12:49:14 +0100

    lkjl

--- src/main/java/com/survey/repository/SurveyRepository.java
+++ src/main/java/com/survey/repository/SurveyRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
-    //Optional<List<Survey>> findById
+
+    List<Survey> ss
 }
